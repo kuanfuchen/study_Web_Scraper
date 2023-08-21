07fedd86169d576c29b644a1237450b9c14b3520
kuanfuchen <creazy_fu@hotmail.com>
2023-08-21 17:39:34 +0800

    add request

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const cheerio = require('cheerio');
-const axios = require('axios');
+// const axios = require('axios');
+const resquest = require('request');
 const http = require('http');
 const url = 'https://rate.bot.com.tw/xrt?Lang=zh-TW';
 const header = {
@@ -8,39 +9,87 @@ const header = {
   'Access-Control-Allow-Methods': 'PATCH, POST, GET,OPTIONS,DELETE',
   'Content-Type': 'application/json',
 };
+
 const getWebScraper = (req, res)=>{
-  console.log('----------')
   if(req.url === '/crawler'){
     const result = [];
-    axios.get(url).then((response)=>{
-      if(!response.data)return
-      const data = response.data;
-      const $ = cheerio.load(data);
-      const tr = $("tbody tr");
+    resquest({
+      url,
+      method:"GET"
+    },(err, response, body)=>{
+      // console.log(response.statusCode);
+      // console.log(response.headers['content-type']);
+      const $ = cheerio.load(body);
+        const tr = $("tbody tr");
       for(let i = 0 ; tr.length > i; i++){
         const coinType = tr.eq(i).find('.hidden-phone.print_show').text().trim();
         const buyCashCoinPrice = tr.eq(i).children().eq(1).text();
         const sellCashCoinPrice = tr.eq(i).children().eq(2).text();
         const buySpotRateCoinPrice = tr.eq(i).children().eq(3).text().trim();
         const sellSpotRateCoinRate = tr.eq(i).children().eq(4).text().trim();
-        if(i===0) console.log(tr.eq(0).children().eq(3));
+        if(i === 0) console.log(tr.eq(0).children().eq(3));
         result.push({
-          '種類':coinType,
-          '現金買入':buyCashCoinPrice,
-          '現金賣出':sellCashCoinPrice,
-          '即期匯率買入':buySpotRateCoinPrice,
-          '即期匯率賣出':sellSpotRateCoinRate,
+          'type':coinType,
+          'cash buy':buyCashCoinPrice,
+          'cash sell':sellCashCoinPrice,
+          'spot rate buy':buySpotRateCoinPrice,
+          'spot rate sell':sellSpotRateCoinRate,
         });
-      }
+        const sellSpotRateCoinRateToNumber = Number(sellSpotRateCoinRate)
+        // console.log(typeof sellSpotRateCoinRate,'buySpotRateCoinPriceToNumber')
+        if(coinType.indexOf('美金') !== -1 && sellSpotRateCoinRateToNumber > 32){
+          console.log({
+            'type':coinType,
+            'cash buy':buyCashCoinPrice,
+            'cash sell':sellCashCoinPrice,
+            'spot rate buy':buySpotRateCoinPrice,
+            'spot rate sell':sellSpotRateCoinRate,
+          })
+        }
+      };
+      // }
       res.writeHeader(200,header);
       res.write(JSON.stringify({
       data:result,
       status:'success'
     }));
     res.end();
-    })
-    
+  })
+    // axios.get(url).then((response)=>{
+    //   if(!response.data)return
+    //   const data = response.data;
+    //   const $ = cheerio.load(data);
+    //   const tr = $("tbody tr");
+    //   for(let i = 0 ; tr.length > i; i++){
+    //     const coinType = tr.eq(i).find('.hidden-phone.print_show').text().trim();
+    //     const buyCashCoinPrice = tr.eq(i).children().eq(1).text();
+    //     const sellCashCoinPrice = tr.eq(i).children().eq(2).text();
+    //     const buySpotRateCoinPrice = tr.eq(i).children().eq(3).text().trim();
+    //     const sellSpotRateCoinRate = tr.eq(i).children().eq(4).text().trim();
+    //     if(i === 0) console.log(tr.eq(0).children().eq(3));
+    //     result.push({
+    //       '種類':coinType,
+    //       '現金買入':buyCashCoinPrice,
+    //       '現金賣出':sellCashCoinPrice,
+    //       '即期匯率買入':buySpotRateCoinPrice,
+    //       '即期匯率賣出':sellSpotRateCoinRate,
+    //     });
+    //   };
+    //     console.log(result,'result')
+    //     res.writeHeader(200,header);
+    //     res.write(JSON.stringify({
+    //     data:result,
+    //     status:'success'
+    //   }));
+    //   res.end();
+    // })
   }
 }
-http.createServer(getWebScraper).listen(8086);
+const repeatUsedTransfer = (req,res)=>{
+  setInterval(() => {
+    getWebScraper(req,res)
+  }, 10000);
+}
+
+http.createServer(repeatUsedTransfer).listen(8086);
 
